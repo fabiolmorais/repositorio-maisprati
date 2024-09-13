0786862e64a1ec3817882f50e4ddde3926471642
Fabio Morais <fabio.lmorais99@gmail.com>
2024-09-13 20:36:50 -0300

    Atualização na função removeItemBtn para que quando o item da lista for igual a zero ele já aparece o texto

--- 2-html-css-js/lesson-4-js-basic/ex-proposto-lesson-1/script.js
+++ 2-html-css-js/lesson-4-js-basic/ex-proposto-lesson-1/script.js
@@ -3,6 +3,7 @@ const addItemBtn = document.getElementById("addItemBtn");
 const removeItemBtn = document.getElementById("removeItemBtn");
 const itemList = document.getElementById("itemList");
 
+
 addItemBtn.addEventListener("click", () => {
   const li = document.createElement("li");
   li.textContent = `Item ${itemList.children.length + 1}`;
@@ -19,6 +20,11 @@ removeItemBtn.addEventListener("click", () => {
     titulo.textContent = "Item removido!";
     titulo.className = "vermelho";
   } else {
+    titulo.textContent = "Não tem mais item à remover!";
+    titulo.className = "preto";
+  }
+
+  if (itemList.children.length === 0 || itemList.children.length < 0) {
     titulo.textContent = "Não tem mais item à remover!";
     titulo.className = "preto";
   }
