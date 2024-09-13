5b91cce1fee3863537a457397d19f5498a7d7f3e
Fabio Morais <fabio.lmorais99@gmail.com>
2024-09-13 20:23:34 -0300

    Aula completa com o desafio proposto concluido

---
+++ 2-html-css-js/lesson-4-js-basic/ex-proposto-lesson-1/script.js
@@ -0,0 +1,25 @@
+const titulo = document.getElementById("titulo");
+const addItemBtn = document.getElementById("addItemBtn");
+const removeItemBtn = document.getElementById("removeItemBtn");
+const itemList = document.getElementById("itemList");
+
+addItemBtn.addEventListener("click", () => {
+  const li = document.createElement("li");
+  li.textContent = `Item ${itemList.children.length + 1}`;
+  itemList.appendChild(li);
+
+  titulo.textContent = `Você adicionou ${itemList.children.length} item(s)!`;
+  titulo.className = "verde";
+});
+
+removeItemBtn.addEventListener("click", () => {
+  if (itemList.children.length > 0) {
+    itemList.removeChild(itemList.lastChild);
+
+    titulo.textContent = "Item removido!";
+    titulo.className = "vermelho";
+  } else {
+    titulo.textContent = "Não tem mais item à remover!";
+    titulo.className = "preto";
+  }
+});
